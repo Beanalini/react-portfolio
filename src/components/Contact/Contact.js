2af0f14898076c5e7cb96d654f73c5ff286f8b99
Beanalini <dat826@gmail.com>
2023-04-05 15:57:59 +0100

    Added EmailJs email service to the contacts form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-//import './style.css';
+import React, { useRef, useState } from 'react';
+import emailjs from "@emailjs/browser";
 
-// Here we import a helper function that will check if the email is valid
+// Helper function that will check if the email is valid
 import { validateEmail } from '../../utils/helpers';
 
 function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
+  // Create state variables for form fields  
   const [email, setEmail] = useState('');
   const [name, setUserName] = useState('');
   const [message, setMessage] = useState('');
@@ -14,14 +13,46 @@ function Form() {
   const [errorMessage, setErrorMessage] = useState('');
   //Error state message for submit
   const [submitMessage, setSubmitMessage] = useState('');
+  const form = useRef();
+  const [showModal,setShowModal] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_jhc86xq",
+        "template_n0stj4j",
+        form.current,
+        "5VAEcA8NIjaS9liyw"
+      )
+      .then(
+        () => {         
+          
+          setShowModal(false);    
+          
+        
+        },
+        (error) => {
+          alert("There was a problem sending your message.", error);
+        }
+        
+      );
+  };
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
+    // Getting the value and name of the input that triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
+
+    console.log(submitMessage.length);
+
+    //if submit message exists remove from screen
+   if( submitMessage.length > 0 ) setSubmitMessage('');
     
-    // Based on the input type, we set the state of either email, username, and password
+       
+    // Based on the input type, we set the state of either email, name, and message
     if (inputType === 'email') {     
       console.log(email);
         const isValid = validateEmail(inputValue);
@@ -41,26 +72,34 @@ function Form() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
     
-    if(!email.length) console.log("not email " +email);
-    if(!name.length) console.log("not name" + name);
+    if(!email.length) console.log("no email " +email);
+    if(!name.length) console.log("no name" + name);
     if(!message.length) console.log("no message" +message);
     console.log(errorMessage);
     if((!email)||(!name)||(!message)) {
-      setSubmitMessage('Please complete all forms fields before submitting');
+      //clear any form field error messages
+      setErrorMessage('');
+      //Display form submission incomplete message
+      setSubmitMessage('Please complete all form fields before submitting');
 
     } else {
-      setSubmitMessage(`Thank you for your message, ${name}`);
+      setSubmitMessage('');
+      console.log(name);
+      setShowModal(true)
+      sendEmail(e);
+      //reset state 
+      setUserName('');
+      setMessage('');
+      setEmail('');
+    
 
+      //reset form fields
+      e.target.reset();
     }
     
     console.log(email, name, message);
     
-    setUserName('');
-    setMessage('');
-    setEmail('');
-    //set timeout to clear submit form message
-    setTimeout(()=> {setSubmitMessage('')}, 1000);
-    //setSubmitMessage('');
+    
     
   };
   
@@ -69,9 +108,9 @@ function Form() {
         <div className='container'>
           <div className="card m-4  w-75 align-items-center align-self-center d-flex border-0" style={{backgroundColor:"#1F3330"}}>            
             <div className="card-body" style={{backgroundColor:"#1F3330"}}>   
-            <h5 className="card-title"></h5>
-                    <p className="card-text fs-5">Thank you for taking the time to look through my portfolio! If you would like further information, please message me using the form below or hit the links at the bottom of the page. </p>        
-                <form className="form input-group mt-3">
+            {/* <h5 className="card-title"></h5> */}
+                    <p className="card-text fs-5">Thank you for taking the time to look through my portfolio! If you would like further information please message me using the form below, or hit the links at the bottom of the page. </p>        
+                <form ref={form} onSubmit={handleFormSubmit} className="form input-group mt-3">
                 <label htmlFor="contact-name" class="form-label">Name</label>
                     <div className="mb-3 input-group">
                       <input
@@ -82,7 +121,8 @@ function Form() {
                       placeholder="Enter your name"
                       className="form-control"
                     />
-                    </div>
+                    
+                    </div>                    
                     <label htmlFor="contact-email" class="form-label">Email address</label>
                     <div className="mb-3 input-group">
                     
@@ -106,19 +146,33 @@ function Form() {
                         rows="10"
                         className="form-control"
                       />
+                       <button type="submit" value="send" className="btn btn-dark">Send</button>    
                       </div>
-                      <button type="button" class=" btn btn-primary" onClick={handleFormSubmit}>Submit</button>        
-                 </form>
-              {errorMessage && (
-                <div className="mt-3">
-                  <p className="error-text">{errorMessage}</p>
-                </div>
-              )}
-              {submitMessage && (
-                <div className="mt-3">
-                  <p className="error-text">{submitMessage}</p>
-                </div>
-              )}
+                         
+                 
+                      {errorMessage && (
+                        <div className="mt-3">
+                          <p className="error-text">{errorMessage}</p>
+                        </div>
+                      )}
+                      {submitMessage && (
+                        <div className="mt-3">
+                          <p className="error-text">{submitMessage}</p>
+                        </div>
+                      )}
+                      </form>
+                      {showModal && (
+                        <div className="modal fade show d-block" tabIndex={-1}>
+                          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                            <div className="modal-content bg-black border border-dark">
+                              <div className="modal-body text-center py-4">
+                                <p>Thank you for your message {name}. I'll get back to you as soon as possible.</p>                                
+                                <button type="button" onClick={() => setShowModal(false)} className="btn btn-dark px-5" data-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+        )}
           </div>
        </div>
       </div>
@@ -126,8 +180,4 @@ function Form() {
 }
 
 export default Form;
-// if(!inputValue.length){
-//   setErrorMessage('Please enter an email address');
-
-// }else{
 
